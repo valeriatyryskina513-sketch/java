08ff8907b96f2cf1ca30e08e8c2dd390cf2e685b
valeriatyryskina513-sketch <valeriatyryskina513@gmail.com>
2026-06-02 20:20:14 +0300

    Update HashTable.java

--- 3/HashTable.java
+++ 3/HashTable.java
@@ -1,1 +1,83 @@
+import java.util.LinkedList;
 
+public class HashTable<K, V> {
+
+    private static class Entry<K, V> {
+        private final K key;
+        private V value;
+
+        public Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private final LinkedList<Entry<K, V>>[] table;
+    private final int capacity;
+    private int size;
+
+    @SuppressWarnings("unchecked")
+    public HashTable(int capacity) {
+        this.capacity = capacity;
+        this.size = 0;
+        this.table = new LinkedList[capacity];
+
+        for (int i = 0; i < capacity; i++) {
+            table[i] = new LinkedList<>();
+        }
+    }
+
+    private int hash(K key) {
+        return Math.abs(key.hashCode()) % capacity;
+    }
+
+    public void put(K key, V value) {
+        int index = hash(key);
+
+        for (int i = 0; i < table[index].size(); i++) {
+            Entry<K, V> entry = table[index].get(i);
+            if (entry.key.equals(key)) {
+                entry.value = value;
+                return;
+            }
+        }
+
+        table[index].add(new Entry<>(key, value));
+        size++;
+    }
+
+    public V get(K key) {
+        int index = hash(key);
+
+        for (int i = 0; i < table[index].size(); i++) {
+            Entry<K, V> entry = table[index].get(i);
+            if (entry.key.equals(key)) {
+                return entry.value;
+            }
+        }
+        return null;
+    }
+
+    public V remove(K key) {
+        int index = hash(key);
+
+        for (int i = 0; i < table[index].size(); i++) {
+            Entry<K, V> entry = table[index].get(i);
+            if (entry.key.equals(key)) {
+                V oldValue = entry.value;
+                table[index].remove(i);
+                size--;
+                return oldValue;
+            }
+        }
+        return null;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+}
