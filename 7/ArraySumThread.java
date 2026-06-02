952e99c8453305eb7ba248b481424122f52d9834
valeriatyryskina513-sketch <valeriatyryskina513@gmail.com>
2026-06-02 21:00:50 +0300

    Create ArraySumThread.java

---
+++ 7/ArraySumThread.java
@@ -0,0 +1,23 @@
+public class ArraySumThread extends Thread {
+    private int[] array;
+    private int start;
+    private int end;
+    private int sum;
+
+    public ArraySumThread(int[] array, int start, int end) {
+        this.array = array;
+        this.start = start;
+        this.end = end;
+        this.sum = 0;
+    }
+
+    public void run() {
+        for (int i = start; i < end; i++) {
+            sum += array[i];
+        }
+    }
+
+    public int getSum() {
+        return sum;
+    }
+}
